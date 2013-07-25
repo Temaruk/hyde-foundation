4b92bb62ea009160ba293e2e50f37345b8ffdda6
Gergely Tamas Kurucz <temaruk@gmail.com>
2013-07-25 18:41:56 +0200

    Fixed update script.

--- update_foundation.sh
+++ update_foundation.sh
@@ -4,8 +4,12 @@
 # https://github.com/auzigog/hyde-bootstrap/blob/master/update-bootstrap-libs.sh
 
 echo "Fetching foundation from github..."
-git submodule update --recursive
+cd foundation
+git checkout scss-standalone
+git pull
+git submodule update --init --recursive
 
 echo "Copying foundation js and scss files under media..."
+cd ..
 rsync -r `pwd`/foundation/js/* `pwd`/site/content/media/js
 rsync -r `pwd`/foundation/scss/* `pwd`/site/content/media/scss
